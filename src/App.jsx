60f1200e8feb4734bdb9a23d608b421786ee268d
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-11-21 16:24:38 +0530

    add new fetchers

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ export default function App() {
     img: "https://purepng.com/public/uploads/large/purepng.com-orange-orangeorangefruitbitter-orangeorangesclip-art-1701527337098cr1i6.png",
   };
   const [text, setText] = React.useState("");
+  const [color, setColor] = React.useState("#ff0000");
   function handleText(event) {
     setText(event.target.value);
   }
@@ -23,6 +24,19 @@ export default function App() {
   function handleClear() {
     setText("");
   }
+
+  function changeColor(event) {
+    setColor(event.target.value);
+  }
+
+  const PrintFirstLetter = () => {
+    text.split(" ");
+    for (let i = 0; i < text.length; i++) {
+      let firstLetter = text[i][0];
+      setText(firstLetter);
+    }
+  };
+
   return (
     <div>
       <div className="container">
@@ -30,7 +44,8 @@ export default function App() {
         <div className="form-group pt-14 ">
           <textarea
             onChange={handleText}
-            className="form-control border-2 text-2xl placeholder:text-gray-500 p-3"
+            style={{ color: color }}
+            className={`form-control border-2 text-2xl placeholder:text-gray-500 p-3 font-mono`}
             id="exampleFormControlTextarea1"
             rows="7"
             placeholder="You can write the something here..."
@@ -60,6 +75,31 @@ export default function App() {
           >
             Clear Text
           </button>
+          <button
+            type="button"
+            className="btn btn-warning mt-4 p-4"
+            onClick={PrintFirstLetter}
+          >
+            PrintFirstLetter
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary mt-4 p-4 flex items-center flex-col gap-2"
+          >
+            <input
+              type="color"
+              id="favcolor"
+              name="favcolor"
+              // value="#ff0000"
+              onChange={changeColor}
+              value={color}
+            />
+            Color Picker
+          </button>
+        </div>
+        <div className="mt-6">
+          <h1 className="h1"> {text.split(" ").length}</h1>
+          <h1 className="h1"> {0.008 * text.split(" ").length}</h1>
         </div>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FaSun } from "react-icons/fa";
+import { MdOutlineNightlightRound } from "react-icons/md";
 
 export default function Header(props) {
+  const [theme, setTheme] = React.useState("bg-dark");
+
   return (
-    <header className="w-full bg-slate-100 p-3 flex justify-between px-5">
+    <header
+      className={`w-full bg-slate-100 p-3 flex justify-between px-5 ${theme} `}
+    >
       <img src={props.img} alt="" className="w-14" />
 
       <ul className="flex text-xl items-center gap-4 uppercase">
-        <li className="cursor-pointer hover:text-blue-400 transition-all">
-          home
+        <li
+          className={`cursor-pointer hover:text-blue-400 ${
+            theme == "bg-dark" ? "text-white" : "text-black"
+          }  transition-all`}
+        >
+          <FaSun
+            color="orange"
+            className="text-5xl hover:spin"
+          />
         </li>
       </ul>
 
-      <ul className="flex text-xl items-center gap-4">
-        <li className="cursor-pointer hover:text-blue-400 transition-all group ">
-          Login
-          <hr className="hidden group-hover:flex border-4" />
-        </li>
-        <li className="cursor-pointer hover:text-blue-400 transition-all group">
-          Register
-          <hr className="hidden group-hover:flex border-4" />
-        </li>
-      </ul>
+      {/* <ul className="flex text-xl items-center gap-4"></ul> */}
     </header>
   );
 }
